70b99994acdeb7d94f32a55350080172dc92ac3f
Franck ludovic <franckludovic351@gmail.com>
2026-05-12 18:59:27 +0100

    setup of the  project

---
+++ src/main/java/com/iut_dla/SalaryGest/SalaryGestApplication.java
@@ -0,0 +1,13 @@
+package com.iut_dla.SalaryGest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryGestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryGestApplication.class, args);
+	}
+
+}
